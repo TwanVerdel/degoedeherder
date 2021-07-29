a4708abd4a90e2836a82ecb3b60b0c79341a6b28
Jochem <jochembrans@gmail.com>
2021-07-29 19:21:18 +0200

    Introduction page

---
+++ components/IntroductionTextBlock.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container">
+        <div class="header">
+            {{title}}
+        </div>
+        <div class="text">
+            {{text}}
+        </div>
+        <div v-if="hasButton">
+            <div class="button">
+                {{buttonText}}
+                <img src="~assets/icons/arrow-right.svg" alt="arrow">
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type: String,
+            default: 'Differences with the English culture'
+        },
+        text:{
+            type: String,
+            default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at dolor sit amet nibh egestas vehicula. Morbi id nibh id lorem tristique bibendum. Donec vel lectus placerat, efficitur neque ut, efficitur nisl. Maecenas dictum massa volutpat placerat eleifend. Curabitur metus arcu, vehicula ac ipsum id, fermentum lacinia ipsum. '
+        },
+        hasButton:{
+            type: Boolean,
+            default: false
+        },
+        buttonText:{
+            type: String,
+            default: 'Read more'
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.container{
+    height: vw(368px);
+    background-color: $secondary;
+    padding: vw(48px);
+
+    @media($phone){
+            height: 100%;
+        }
+
+    @media($tablet-portrait){
+            height: 100%;
+        }
+
+    .header{
+        font-size: vw(28px);
+        font-weight: bold;
+        margin-bottom: vw(24px);
+    }
+
+    .text{
+        font-size: vw(18px);
+        line-height: 150%;
+        margin-bottom: vw(24px);
+    }
+    .button{
+        width: vw(176px);
+        height: vw(56px);
+        background-color: $primary;
+        border-radius: vw(56px);
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+        color: $white;
+        font-size: vw(18px);
+
+        img{
+            height: vw(16px);
+            width: vw(16px);
+            margin-left: vw(12px);
+        }
+
+    }
+}
+</style>
---
+++ pages/introductie.vue
@@ -0,0 +1,67 @@
+<template>
+    <main class="page">
+        <div class="header">
+            <h1>
+                Introduction to the
+                <br />parish of Emmen.
+            </h1>
+        </div>
+        <div class="banner">
+            <img src="https://picsum.photos/1000" alt="placeholder">
+        </div>
+        <div class="grid">
+            <introduction-text-block :hasButton="true" />
+            <introduction-text-block :hasButton="true" />
+            <introduction-text-block :hasButton="false" />
+            <introduction-text-block :hasButton="false" />
+        </div>
+    </main>
+</template>
+
+<script>
+import IntroductionTextBlock from "../components/IntroductionTextBlock";
+
+export default {
+    components: { IntroductionTextBlock }
+};
+</script>
+
+<style lang="scss" scoped>
+.page {
+    @include Outer(112px, 112px);
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .header{
+        text-align: center;
+        margin-top: vw(64px);
+        margin-bottom: vw(96px);
+    }
+
+    .banner{
+        position: relative;
+        height: vw(368px);
+        margin-bottom: vw(80px);
+
+        img{
+            position: absolute;
+            left: calc(var(--outer) * -1);
+            width: 100vw;
+            max-height: vw(368px);
+            object-fit: cover;
+        }
+    }
+
+    .grid {
+        display: grid;
+        gap: vw(32px);
+        grid-template-columns: 1fr 1fr;
+        width: 100%;
+
+        @media ($phone) {
+            grid-template-columns: 1fr;
+        }
+    }
+}
+</style>
