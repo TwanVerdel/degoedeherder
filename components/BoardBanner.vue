19e8baaf1cb33e6b764a42cd3043cf08d89a17b7
BramGerrits <bramgggeee@gmail.com>
2021-08-30 21:43:52 +0200

    boardbanner geupdate

--- components/BoardBanner.vue
+++ components/BoardBanner.vue
@@ -4,20 +4,31 @@
     <section class="board-banner">
         <div class="info-container">
             <h2>Ontdek het geloof</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ultricies efficitur lacus. Etiam tincidunt magna diam, porta dignissim sapien suscipit quis. Vestibulum pharetra finibus dolor, at malesuada mauris varius nec. Aenean facilisis metus sem, ut luctus lectus imperdiet in. Integer feugiat erat felis.</p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                ultricies efficitur lacus. Etiam tincidunt magna diam, porta
+                dignissim sapien suscipit quis. Vestibulum pharetra finibus
+                dolor, at malesuada mauris varius nec. Aenean facilisis metus
+                sem, ut luctus lectus imperdiet in. Integer feugiat erat felis.
+            </p>
         </div>
 
         <div v-if="hasMembers" class="member-container">
-            <div class="members">
-                <img src="~assets/images/double-quote.svg" alt="Quote" />
-                <div v-for="member in members" :key="member" class="boxed">{{member}}</div>
-            </div>
+            <ul class="members">
+                <!-- <img src="~assets/images/double-quote.svg" alt="Quote" /> -->
+                <li
+                    v-for="member in members"
+                    :key="member"
+                    class="boxed"
+                    v-text="member"
+                />
+            </ul>
         </div>
         <div v-if="!hasMembers" class="member-container">
             <img src="~assets/images/double-quote.svg" alt="Quote" />
             <div class="text">
                 <p>
-                    <span>{{text}}</span>
+                    <span>{{ text }}</span>
                 </p>
             </div>
         </div>
@@ -31,13 +42,13 @@ export default {
     props: {
         hasMembers: {
             type: Boolean,
-            default: false
+            default: false,
         },
         text: {
             type: String,
             default:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis lectus sed nisi iaculis auctor. Donec eu nibh rhoncus. "
-        }
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin facilisis lectus sed nisi iaculis auctor. Donec eu nibh rhoncus. ",
+        },
     },
     data() {
         return {
@@ -46,10 +57,10 @@ export default {
                 "B. Gerrits",
                 "T. Verdel",
                 "J. Brans",
-                "B. Gerrits"
-            ]
+                "B. Gerrits",
+            ],
         };
-    }
+    },
 };
 </script>
 
@@ -57,8 +68,9 @@ export default {
 
 <style lang="scss" scoped>
 .board-banner {
-    @include Outer;
+    @include Outer(128px);
     display: flex;
+    align-items: center;
     flex-direction: row;
 
     @media ($tablet-portrait) {
@@ -68,10 +80,6 @@ export default {
     /*Variables*/
     background: $tertiary;
     width: 100%;
-
-    /*Padding*/
-    padding-top: desktop-vw(80px);
-    padding-bottom: desktop-vw(72px);
 
     @media ($tablet-portrait) {
         padding-top: tablet-vw(80px);
@@ -121,6 +129,15 @@ export default {
                 text-align: center;
             }
         }
+
+        p {
+            max-width: vw(640px);
+
+            @media ($tablet-portrait) {
+                margin-left: auto;
+                margin-right: auto;
+            }
+        }
     }
 
     .member-container {
@@ -139,35 +156,44 @@ export default {
             width: 100%;
             padding-left: 0%;
             padding-right: 0%;
+            margin-top: vw(24px);
         }
 
         .members {
+            position: relative;
             display: flex;
             flex-direction: row;
             flex-wrap: wrap;
             justify-content: center;
             max-width: vw(300px);
-        }
 
-        img {
-            display: flex;
-            width: vw(16px);
-            height: vw(16px);
-        }
+            &::before {
+                content: '“';
+                
+                position: absolute;
+                left: vw(8px);
+                top: 0;
 
-        .boxed {
-            background-color: $white;
-            padding: vw(8px);
-            height: min-content;
-            margin: vw(4px);
+                font-family: Roboto;
+                font-size: vw(80px);
+                line-height: 100%;  
+                transform: translateX(-100%);
+                color: $white;
+            }
 
-            min-width: min-content;
+            .boxed {
+                background-color: $white;
+                padding: vw(8px);
+                height: min-content;
+                margin: vw(4px);
+                min-width: min-content;
+                font-style: italic;
+                list-style: none;
 
-            font-style: italic;
-
-            p {
-                font-weight: 400px;
-                font-size: vw(24px);
+                p {
+                    font-weight: 400px;
+                    font-size: vw(24px);
+                }
             }
         }
 
