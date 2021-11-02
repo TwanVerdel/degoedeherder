65e4861a1a9a3bb1e6eccb0690862e9d7aa2a01b
BramGerrits <bramgggeee@gmail.com>
2021-11-02 20:09:27 +0100

    financien component

---
+++ pages/financiën.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <finances-list />
+    </section>
+</template>
+
+<script>
+    import FinancesList from '../components/FinancesList.vue'
+
+    export default {
+        components: { 
+            FinancesList 
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
