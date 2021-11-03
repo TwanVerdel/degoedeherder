9b3764904f7d94734f251a44040244b9bdf68a33
BramGerrits <bramgggeee@gmail.com>
2021-11-03 20:36:01 +0100

    collapsableWYSIWYG component

---
+++ components/CollapsableWYSIWYG.vue
@@ -0,0 +1,201 @@
+<template>
+    <section
+        class="container"
+        :class="{
+            overflows: overflows,
+            show: show,
+        }"
+    >
+        <div
+            class="content"
+            ref="contentContainer"
+            :style="`--grow-height: ${growHeight}px;`"
+        >
+            <slot />
+        </div>
+        <button
+            v-if="overflows"
+            class="show-toggle-button"
+            @click="toggleShow()"
+        >
+            <span class="more"> Meer weergeven </span>
+            <span class="less"> Minder weergeven </span>
+        </button>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            overflows: false,
+            show: false,
+            growHeight: 0,
+        };
+    },
+    computed: {
+        container() {
+            return this.$refs.contentContainer;
+        },
+    },
+    methods: {
+        toggleShow() {
+            this.setGrowHeight();
+
+            this.show = !this.show;
+
+            if (this.show === false) {
+                this.scrollToContainer();
+            }
+        },
+
+        scrollToContainer() {
+            setTimeout(() => {
+                window.scrollTo({
+                    top: this.container.offsetTop - 300,
+                    behavior: "smooth",
+                });
+            }, 200);
+        },
+
+        setGrowHeight() {
+            this.growHeight = this.container.scrollHeight || 0;
+        },
+
+        checkIfOverflows() {
+            this.overflows = this.container.scrollHeight > this.container.offsetHeight;
+        },
+    },
+    mounted() {
+        this.setGrowHeight();
+        this.checkIfOverflows();
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: vw(120px) 0 vw(88px) 0;
+
+    @media ($tablet-portrait) {
+        padding: vw(80px) 0 vw(56px) 0;
+    }
+
+    .content {
+        --grow-height: 0;
+
+        position: relative;
+        max-height: vw(240px);
+        overflow: hidden;
+        width: vw(760px);
+        transition: max-height 0.8s;
+
+        @media ($tablet-portrait) {
+            width: 100%;
+            padding-left: var(--outer);
+            padding-right: var(--outer);
+        }
+
+        &::v-deep {
+            &::before {
+                content: "";
+                height: vw(120px);
+                width: 100%;
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                background: linear-gradient(
+                    180deg,
+                    rgba($white, 0) 0%,
+                    $white 100%
+                );
+                transition: opacity 0.4s;
+            }
+
+            * {
+                margin-bottom: 0.5em;
+                margin-top: 1em;
+            }
+
+            img {
+                margin-bottom: vw(56px);
+                margin-top: vw(56px);
+                height: vw(240px);
+                width: 100%;
+                object-position: center;
+                object-fit: contain;
+            }
+
+            *:first-child {
+                margin-top: 0;
+            }
+
+            *:last-child {
+                margin-bottom: 0;
+            }
+        }
+    }
+
+    .show-toggle-button {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        height: vw(48px);
+        width: vw(180px);
+        background: transparent;
+
+        transition: transform 0.2s;
+        cursor: pointer;
+        margin-top: vw(16px);
+
+        &:hover {
+            transform: translateY(vw(4px));
+        }
+
+        span {
+            position: absolute;
+            transition: opacity 0.2s;
+            font-weight: bold;
+        }
+    }
+}
+
+.container:not(.show) {
+    .show-toggle-button {
+        span.less {
+            opacity: 0;
+        }
+    }
+}
+
+.container.show {
+    .content {
+        max-height: var(--grow-height);
+        // max-height: vw(1000px);
+
+        &::before {
+            opacity: 0;
+            pointer-events: none;
+        }
+    }
+
+    .show-toggle-button {
+        span.more {
+            opacity: 0;
+        }
+    }
+}
+
+.container:not(.overflows) {
+    .content {
+        &::before {
+            opacity: 0;
+            pointer-events: none;
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -11,10 +11,80 @@
             :imgAlt="'Mooi beeld'"
         />
         <article-grid showThree type="nieuws" :articles="news" />
-        <article-grid showThree :title="'Pastoorblogs'" :articles="blogs"/>
+        <article-grid showThree :title="'Pastoorblogs'" :articles="blogs" />
 
-        <alternate-content title="Bisdomnieuws" description="De parochies van Erica, Barger-Oosterveld, Emmerschans en Emmen maken deel uit van het bisdom Groningen-Leeuwarden. Klik hieronder voor het laatste nieuws." buttonText="Bisdomnieuws" buttonURL="https://bisdomgl.nl/categorie/nieuws/"/>
+        <alternate-content
+            title="Bisdomnieuws"
+            description="De parochies van Erica, Barger-Oosterveld, Emmerschans en Emmen maken deel uit van het bisdom Groningen-Leeuwarden. Klik hieronder voor het laatste nieuws."
+            buttonText="Bisdomnieuws"
+            buttonURL="https://bisdomgl.nl/categorie/nieuws/"
+        />
         <!-- <board-banner :hasMembers="true" /> -->
+
+        <collapsable-wysiwyg>
+            <h2>Weekendvieringen.</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent sodales vitae mi viverra eleifend. Etiam non viverra
+                tellus. Nulla sit amet maximus sapien, a volutpat justo. Donec
+                blandit fringilla dictum. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Maecenas tortor risus, fermentum
+                vitae quam elementum, hendrerit semper augue. Morbi eget enim
+                eros. Cras in massa molestie est imperdiet sollicitudin. Nullam
+                pulvinar leo ipsum, ac scelerisque justo congue sit amet. Cras
+                eget lacinia tellus. Nullam laoreet euismod nisi, ac molestie
+                sem auctor et. Cras sed metus velit. Sed laoreet neque erat,
+                eget dictum nibh volutpat vitae. Etiam vel magna commodo,
+                convallis libero et, hendrerit eros.
+            </p>
+            <p>
+                Ut urna odio, commodo non iaculis a, finibus eget nisi. Aenean
+                vehicula mollis urna, id mattis massa aliquam at. Proin
+                malesuada augue nec blandit dignissim. Maecenas molestie ligula
+                augue, id eleifend felis ultricies quis. Nam dictum tempus
+                ultricies. Nunc interdum varius tincidunt. Nam purus mi, dapibus
+                vel eleifend vitae, cursus et est. Praesent id molestie mi,
+                ullamcorper lobortis massa. Morbi fermentum augue ac arcu
+                placerat, ac aliquet nisi tristique. Sed et eros blandit,
+                dignissim urna accumsan, bibendum tortor. Donec faucibus turpis
+                vitae diam feugiat, mollis semper lectus pulvinar. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus. Integer mollis
+                diam felis, eu accumsan magna tristique eget.
+            </p>
+
+            <img src="https://picsum.photos/900/500" />
+
+            <h2>Weekendvieringen.</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Praesent sodales vitae mi viverra eleifend. Etiam non viverra
+                tellus. Nulla sit amet maximus sapien, a volutpat justo. Donec
+                blandit fringilla dictum. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Maecenas tortor risus, fermentum
+                vitae quam elementum, hendrerit semper augue. Morbi eget enim
+                eros. Cras in massa molestie est imperdiet sollicitudin. Nullam
+                pulvinar leo ipsum, ac scelerisque justo congue sit amet. Cras
+                eget lacinia tellus. Nullam laoreet euismod nisi, ac molestie
+                sem auctor et. Cras sed metus velit. Sed laoreet neque erat,
+                eget dictum nibh volutpat vitae. Etiam vel magna commodo,
+                convallis libero et, hendrerit eros.
+            </p>
+            <h3>Weekendvieringen.</h3>
+            <p>
+                Ut urna odio, commodo non iaculis a, finibus eget nisi. Aenean
+                vehicula mollis urna, id mattis massa aliquam at. Proin
+                malesuada augue nec blandit dignissim. Maecenas molestie ligula
+                augue, id eleifend felis ultricies quis. Nam dictum tempus
+                ultricies. Nunc interdum varius tincidunt. Nam purus mi, dapibus
+                vel eleifend vitae, cursus et est. Praesent id molestie mi,
+                ullamcorper lobortis massa. Morbi fermentum augue ac arcu
+                placerat, ac aliquet nisi tristique. Sed et eros blandit,
+                dignissim urna accumsan, bibendum tortor. Donec faucibus turpis
+                vitae diam feugiat, mollis semper lectus pulvinar. Interdum et
+                malesuada fames ac ante ipsum primis in faucibus. Integer mollis
+                diam felis, eu accumsan magna tristique eget.
+            </p>
+        </collapsable-wysiwyg>
     </main>
 </template>
 
@@ -30,15 +100,16 @@ import ArticleGrid from "../components/ArticleGrid.vue";
 // import PastorsContent from "../components/PastorsContent.vue";
 import ANBIBadge from "../components/ANBIBadge.vue";
 import Hero from "../components/Hero.vue";
+import CollapsableWYSIWYG from "../components/CollapsableWYSIWYG.vue";
 // import HistoryComponent from "../components/HistoryComponent.vue";
 // import BoardBanner from "../components/BoardBanner.vue";
 
 export default {
-    
     components: {
         Hero,
         ANBIBadge,
-        ArticleGrid
+        ArticleGrid,
+        CollapsableWYSIWYG,
     },
     computed: {
         hero() {
@@ -49,7 +120,7 @@ export default {
         },
         blogs() {
             return this.blogData.pastorBlogs;
-        }
+        },
     },
     layout: "hero",
 
@@ -61,9 +132,9 @@ export default {
         return {
             heroData,
             newsData,
-            blogData
+            blogData,
         };
-    }
+    },
 };
 </script>
 
