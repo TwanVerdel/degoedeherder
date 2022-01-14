a9f938c5df2e317820508c68e589904ffea2cb8c
TwanVerdel <twan.verdel@student.nhlstenden.com>
2022-01-14 22:29:22 +0100

    Donationbanner Finances

--- pages/financien.vue
+++ pages/financien.vue
@@ -8,6 +8,13 @@
         </section>
 
         <section>
+
+        <!-- Doneren algemene kas -->
+        <!-- <donation-banner
+            title="Kerkbijdrage of gift"
+            :description="contribution.html"
+            :buttonURL="paymentURL"
+        /> -->
         <!-- Kerkhof tarieven -->
             <CollapsableWYSIWYG>
                 <h3>{{financeInfo.graveyardHeader}}</h3>
